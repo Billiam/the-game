079ef56f1367097303e42716e5d9919bee435654
Billiam <billiamthesecond@gmail.com>
2016-07-03 21:14:32 -0500

    Adding attack item

--- lib/item_classes.rb
+++ lib/item_classes.rb
@@ -4,7 +4,7 @@ module ItemClasses
   BOOST = ['Moogle', 'Rush the Dog', '7777', 'Warthog', 'Miniature Giant Space Hamster', 'Pony']
   POINTS = ['Treasure Chest', 'Buffalo', 'Biggs', 'Pizza', 'UUDDLRLRBA', 'Pokeball', 'Chocobo', 'Da Da Da Da Daaa Da DAA da da', 'Wedge', 'Skinny Guys', 'Bo Jackson']
   ATTACK = ['Crowbar', 'Red Shell']
-  ATTACK_PLAYER = ['Master Sword', 'Buster Sword', 'Charizard', 'Rail Gun', 'SPNKR', 'Hard Knuckle', 'Box of Bees', "Pandora's Box", 'Hadouken', 'Banana Peel', 'Green Shell', 'Fire Flower', 'Holy Water', 'Golden Gun', 'Fus Ro Dah', 'Box of Bees']
+  ATTACK_PLAYER = ['Master Sword', 'Buster Sword', 'Charizard', 'Rail Gun', 'SPNKR', 'Red Crystal', 'Hard Knuckle', 'Box of Bees', "Pandora's Box", 'Hadouken', 'Banana Peel', 'Green Shell', 'Fire Flower', 'Holy Water', 'Golden Gun', 'Fus Ro Dah', 'Box of Bees']
   
   #Roger Wilco # - You used <Roger Wilco> on cfein; 0 points for cfein!
   
